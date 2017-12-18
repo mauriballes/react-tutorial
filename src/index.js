12e87f13064fea93510789c3f3fc4d1b6a4c8b8f
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-18 18:22:37 -0400

    Using More JSX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function formatName(user){
+    return user.firstname + ' ' + user.lastname
+}
+
+const user = {
+    firstname: 'Mauricio',
+    lastname: 'Ballesteros'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}
+    </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
