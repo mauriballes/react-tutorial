eed53ff61ac7cc0fb60ae827ff1226f78a9dcab9
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-19 10:35:14 -0400

    Creating Components

--- src/index.js
+++ src/index.js
@@ -2,27 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user) {
-    return user.firstname + ' ' + user.lastname
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello {this.props.name}</h1>;
+    }
 }
 
-const user = {
-    firstname: 'Mauricio',
-    lastname: 'Ballesteros'
-};
+const element = <Welcome name="Mauricio" />;
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, {formatName(user)}</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
