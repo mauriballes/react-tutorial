9ac18587eda1abc898972cef5a91766049d0cecf
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-19 16:36:41 -0400

    Conditional Rendering: Inline if with &&

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Clock from './components/Clock';
 import Toggle from './components/Toggle';
 import LoginControl from './components/LoginControl';
+import Mailbox from './components/Mailbox';
+
+const messages = ['React', 'Re: React', 'Re:Re: React', 'Re:Re:Re: React'];
 
 export default class App extends React.Component {
     render() {
@@ -12,6 +15,7 @@ export default class App extends React.Component {
                 <Clock name="Darlyn" />
                 <Toggle />
                 <LoginControl />
+                <Mailbox unreadMessages={messages} />
             </div>
         );
     }
