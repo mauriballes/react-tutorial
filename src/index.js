901168a32a0dbf81583a4ee30de30409e0406fc0
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-19 10:49:16 -0400

    Add Component for Extracting small components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Comment extends React.Component {
+    render() {
+        return (
+            <div className="Comment">
+                <div className="UserInfo">
+                    <img className="Avatar"
+                        src={this.props.author.avatarUrl}
+                        alt={this.props.author.name}
+                    />
+                    <div className="UserInfo-name">
+                        {this.props.author.name}
+                    </div>
+                </div>
+                <div className="Comment-text">
+                    {this.props.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(this.props.date)}
+                </div>
+            </div>
+        );
+    }
+}
+
 class Welcome extends React.Component {
     render() {
         return <h1>Hello {this.props.name}</h1>;
