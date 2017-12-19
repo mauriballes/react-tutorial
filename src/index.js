4a2ad60872bdee5d3ce4e8b5e84b8fc7e236fa5f
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-19 11:25:13 -0400

    Adding Local State

--- src/index.js
+++ src/index.js
@@ -9,23 +9,24 @@ class Welcome extends React.Component {
 }
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
     render() {
         return (
             <div>
                 <Welcome name="Mauricio" />
                 <Welcome name="Nicole" />
                 <Welcome name="Darlyn" />
-                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
 }
 
-function tick() {
-    ReactDOM.render(
-        <Clock date={new Date()} />,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
