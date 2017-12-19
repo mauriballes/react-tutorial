0d1f980237a50667d38c77f8615d9dcab001567b
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-19 16:00:38 -0400

    Conditional Rendering first part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from './components/Clock';
 import Toggle from './components/Toggle';
+import Greeting from './components/Greeting';
 
 export default class App extends React.Component {
     render() {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
                 <Clock name="Nicole" />
                 <Clock name="Darlyn" />
                 <Toggle />
+                <Greeting isLoggedIn={false}/>
             </div>
         );
     }
---
+++ src/components/Greeting.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class UserGreeting extends React.Component {
+    render() {
+        return <h1>Welcome back!</h1>;
+    }
+}
+
+class GuestGreeting extends React.Component {
+    render() {
+        return <h1>Please sign up.</h1>
+    }
+}
+
+export default class Greeting extends React.Component {
+    render() {
+        const isLoggedIn = this.props.isLoggedIn;
+        if (isLoggedIn) {
+            return <UserGreeting />;
+        }
+        return <GuestGreeting />
+    }
+}
